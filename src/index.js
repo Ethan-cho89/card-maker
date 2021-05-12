28157fd46142aa9df981792fc522bf35f30a7da2
ethan <bjaguar89@gmail.com>
2021-05-12 17:52:31 +0900

    m

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.module.css';
 import App from './app';
+import styles from './index.module.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className={styles.window}>
+      <App />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
